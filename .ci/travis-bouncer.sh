283045d2ea7a9db52cbd8678f46e15bca25bf08c
tooomm <tooomm@users.noreply.github.com>
2019-11-12 00:51:07 +0100

    Update travis-bouncer.sh

--- .ci/travis-bouncer.sh
+++ .ci/travis-bouncer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Check changed files
+
+# not yet executable
+# look into `travis_terminate` to stop a travis build from continuing to execute following stages
+# https://medium.com/@manjula.cse/how-to-stop-the-execution-of-travis-pipeline-if-script-exits-with-an-error-f0e5a43206bf
 echo "No content yet in travis-bouncer.sh"
 
 exit 0
